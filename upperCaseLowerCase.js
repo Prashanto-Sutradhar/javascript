02f76e334375ac9c0f14c65a5e357a7490a6c369
prashanto777 <shishirsd777@gmail.com>
2023-08-06 01:05:26 +0600

    upperCase and lowercase in string

---
+++ upperCaseLowerCase.js
@@ -0,0 +1,9 @@
+var userName = "BlackPink";
+var input = "BlackPINK";
+
+if (userName.toUpperCase() === input.toUpperCase()) {
+    console.log("welcome");
+
+} else {
+    console.log("incorret");
+}
