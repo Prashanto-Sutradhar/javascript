547d2625df9429d7022f9b359aa8bafa80afd167
prashanto777 <shishirsd777@gmail.com>
2023-08-07 01:24:59 +0600

    abstraction round ceil floor random

---
+++ mathAbsRound.js
@@ -0,0 +1,50 @@
+// power
+console.log("power")
+
+const result = Math.pow(3, 8);
+console.log(result);
+
+// 
+
+// 
+
+
+// abstraction
+console.log("abstraction")
+
+const num1 = 20;
+const num2 = 45;
+const total1 = (num1 - num2);
+console.log(total1);
+const total = Math.abs(num1 - num2);
+console.log(total);
+
+
+
+// round
+console.log("round")
+
+const num3 = 50.54526
+console.log(Math.round(num3));
+const num4 = 50.4526
+console.log(Math.round(num4));
+
+
+
+
+//ceil and floor
+console.log("ceil and floor")
+
+const num6 = 2.0001;
+console.log(Math.ceil(num6));
+const num7 = 2.0001;
+console.log(Math.floor(num7));
+
+
+
+
+
+// random values
+console.log("random values")
+const num8 = Math.random() * 100;
+console.log(num8);
